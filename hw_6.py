1ebb8a31ce27f042d8614c526ce9279f7a1b7360
Barchynai34 <barchynai3003@gmail.com>
2023-05-06 19:21:32 +0300

    Update add git test file

--- hw_6.py
+++ hw_6.py
@@ -23,7 +23,7 @@
 # reverse_num("923")
 
 
-#Доп.задание
+# Доп.задание
 # n = input("Введите: ")
 # while type(n) = int:
 #     try:
@@ -33,4 +33,16 @@
 #     if n == "Вот так!":
 #         print("Успокойся!")
 
+def chatbot():
+    while True:
+        text=input("Введите что-то:")
+        if text.find("?")>=0:
+            print("Конечно!")
+        elif text.find("!")>=0:
+            print("Успокойся")
+        elif text == "":
+            print("Как классно, когда ты молчишь. Продолжай в том же духе!")
+        else:
+            print("Ну и что")
+chatbot()
         
